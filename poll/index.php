c34b5973f41bafbcd0948a73176b2a5695e5ccd3
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-07-12 20:01:24 +0700

    Fix for ID as Array

--- poll/index.php
+++ poll/index.php
@@ -4,7 +4,7 @@ Asset::set(__DIR__ . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'poll.min.css
 Asset::set(__DIR__ . DS . 'lot' . DS . 'asset' . DS . 'js' . DS . 'poll.min.js');
 
 function fn_poll_path($path, $id) {
-    if (($id === 'poll' || strpos($id, 'poll/') === 0) && !$path) {
+    if (($id === 'poll' || is_string($id) && strpos($id, 'poll/') === 0) && !$path) {
         return __DIR__ . DS . 'lot' . DS . 'worker' . DS . $id . '.php';
     }
     return $path;
